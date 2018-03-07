49ae3d79645b545d59301071e521c0bcbec14015
Saurabh S <saurabh.s0373@gmail.com>
2018-03-08 00:02:37 +0530

    Added Menu Scene

---
+++ Assets/Scripts/Core/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bomberman
+{
+    public class Menu : MonoBehaviour
+    {
+        public Button playButton;
+
+        private void Start()
+        {
+            playButton.onClick.AddListener( () => StartGame() );
+        }
+
+        private void StartGame()
+        {
+            PlayerPrefs.SetInt( Constants.GAME_TYPE, Constants.COOP_ID );
+            UnityEngine.SceneManagement.SceneManager.LoadScene( $"map0{Random.Range( 1, 3 )}" );
+        }
+    }
+}
