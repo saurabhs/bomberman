5e280cdd4e1667db054ece2eb5e416158f7e9596
Saurabh S <saurabh.s0373@gmail.com>
2018-03-09 00:46:58 +0530

    Updated map with enemies and added map03 and map04

--- Assets/Resources/Tilemaps/tilemap.tsx
+++ Assets/Resources/Tilemaps/tilemap.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilemap" tilewidth="96" tileheight="96" tilecount="4" columns="2">
- <image source="tilemap.png" width="192" height="192"/>
+<tileset name="tilemap" tilewidth="96" tileheight="96" tilecount="6" columns="2">
+ <image source="tilemap.v2.png" width="192" height="288"/>
 </tileset>
